b684424a9f83f1cb56590103113a4aedcb03e560
zxyimage <zhangxinyuimage@gmail.com>
2023-04-14 21:38:13 +0800

    modify scripts.js

--- scripts.js
+++ scripts.js
@@ -2,20 +2,34 @@ const apiKey = 'dfd33e9d71ac44d2b0aac74b2d731b27'; // Replace 'YOUR_API_KEY' wit
 const apiUrl = `https://newsapi.org/v2/everything?q=artificial+intelligence&apiKey=${apiKey}`;
 
 async function getNews() {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    displayNews(data.articles);
+  try {
+    const response = await fetch(
+      "https://newsapi.org/v2/everything?q=artificial+intelligence&apiKey=dfd33e9d71ac44d2b0aac74b2d731b27"
+    );
+    if (response.status === 200) {
+      const data = await response.json();
+      if (data && data.articles) {
+        displayNews(data.articles);
+      } else {
+        console.error("Unexpected API response format:", data);
+      }
+    } else {
+      console.error("Failed to fetch news with status:", response.status);
+    }
+  } catch (error) {
+    console.error("Error fetching news:", error);
+  }
 }
 
 function displayNews(articles) {
-    const newsContainer = document.getElementById('news-container');
-    newsContainer.innerHTML = articles.map(article => `
-        <div class="news-item">
-            <img src="${article.urlToImage}" alt="${article.title}">
-            <h3><a href="${article.url}" target="_blank">${article.title}</a></h3>
-            <p>${article.description}</p>
-        </div>
-    `).join('');
+  const newsHTML = articles.map((article) => {
+    return `
+      <div class="news-article">
+        <h3><a href="${article.url}" target="_blank">${article.title}</a></h3>
+        <p>${article.description}</p>
+      </div>
+    `;
+  });
+  document.getElementById("news-container").innerHTML = newsHTML.join("");
 }
 
-getNews();
